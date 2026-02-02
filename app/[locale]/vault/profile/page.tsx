86cad0b1c363e0ad723b9eefd44b8e61d3285c0f
TardisThief <magomezf94@gmail.com>
2026-02-02 09:52:53 -0500

    Update page.tsx

--- app/[locale]/vault/profile/page.tsx
+++ app/[locale]/vault/profile/page.tsx
@@ -45,7 +45,7 @@ export default async function ProfileHub() {
             </div>
 
             {/* Split Grid - Adjusted for vertical compression */}
-            <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 lg:h-[70vh]">
+            <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 lg:min-h-[70vh]">
 
                 {/* Left: Tailor's Card (Approx 35-40%) */}
                 <div className="lg:col-span-5 h-full">
